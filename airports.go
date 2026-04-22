1cadd516436b48588ed9dba00c71535cbd7a3c67
Kim Hang Ngo <ngokimhang1@gmail.com>
2026-04-22 16:15:40 +0300

    Add bonus city names

--- airports.go
+++ airports.go
@@ -6,7 +6,12 @@ import (
 	"os"
 )
 
-func loadAirports(airportPath string) (map[string]string, map[string]string, error) {
+type Airport struct {
+	Name string
+	City string
+}
+
+func loadAirports(airportPath string) (map[string]Airport, map[string]Airport, error) {
 	file, err := os.Open(airportPath)
 	if err != nil {
 		return nil, nil, err
@@ -14,40 +19,47 @@ func loadAirports(airportPath string) (map[string]string, map[string]string, err
 	defer file.Close()
 
 	reader := csv.NewReader(file)
-	records, err:= reader.ReadAll()
+	records, err := reader.ReadAll()
 	if err != nil {
 		return nil, nil, err
 	}
 
 	if len(records) < 1 {
-	return nil, nil, errors.New("empty csv")
+		return nil, nil, errors.New("empty csv")
 	}
 
-	if len(records[0]) < 6 {
-	return nil, nil, errors.New("bad header")
+	expected := []string{"name", "iso_country", "municipality", "icao_code", "iata_code", "coordinates"}
+	for i, col := range expected {
+		if records[0][i] != col {
+			return nil, nil, errors.New("bad header")
+		}
 	}
 
-	iataMap := make(map[string]string)
-	icaoMap := make(map[string]string)
+	iataMap := make(map[string]Airport)
+	icaoMap := make(map[string]Airport)
 
 	for _, row := range records[1:] {
 		if len(row) < 6 {
 			return nil, nil, errors.New("bad row")
-			}
-		
-		for i:=0 ; i < 6; i++ {
+		}
+
+		for i := 0; i < 6; i++ {
 			if row[i] == "" {
 				return nil, nil, errors.New("blank cell")
 			}
 		}
-		
+
 		name := row[0]
-		icao:=row[3]
-		iata:= row[4]
+		city := row[2]
+		icao := row[3]
+		iata := row[4]
 
-		iataMap[iata]=name
-		icaoMap[icao]=name
+		a := Airport{
+			Name: name,
+			City: city,
+		}
+		iataMap[iata] = a
+		icaoMap[icao] = a
 	}
-	return iataMap,icaoMap, nil
+	return iataMap, icaoMap, nil
 }
-
--- transform.go
+++ transform.go
@@ -2,35 +2,46 @@ package main
 
 import (
 	"regexp"
+	"strings"
 )
 
-func replaceAirportCodes(text string, iataMap, icaoMap map[string]string) string {
-	reICAO := regexp.MustCompile(`##[A-Z]{4}`)
-	text = reICAO.ReplaceAllStringFunc(text, func(match string) string {
-		code := match[2:]
-		name, found := icaoMap[code]
-		if found {
-			return name
-		}
-		return match
-	})
+func replaceAirportCodes(text string, iataMap, icaoMap map[string]Airport) string {
+	re := regexp.MustCompile(`\*?##[A-Z]{4}|\*?#[A-Z]{3}`)
+	text = re.ReplaceAllStringFunc(text, func(match string) string {
+		wantCity := false
+		original := match
 
-	reIATA := regexp.MustCompile(`(^|[^#])(#[A-Z]{3})`)
-	text = reIATA.ReplaceAllStringFunc(text, func(match string) string {
-		prefix := ""
-		token := match
-
-		if len(match) > 4 {
-			prefix = match[:1]
-			token = match[1:]
+		if strings.HasPrefix(match, "*") {
+			wantCity = true
+			match = match[1:]
 		}
 
-		code := token[1:]
-		name, found := iataMap[code]
-		if found {
-			return prefix + name
+		var a Airport
+		var found bool
+
+		if strings.HasPrefix(match, "##") {
+			code := match[2:]
+			a, found = icaoMap[code]
+		} else {
+			code := match[1:]
+			a, found = iataMap[code]
 		}
-		return match
+
+		if !found {
+			return original
+		}
+
+		if wantCity {
+			if a.City != "" {
+				return a.City
+			}
+			return original
+		}
+
+		if a.Name != "" {
+			return a.Name
+		}
+		return original
 	})
 	return text
 }
@@ -39,6 +50,16 @@ func normalizeVerticalWhitespace(text string) string {
 	re := regexp.MustCompile(`\x{000D}\x{000A}|[\x{000B}\x{000C}]|(\\r|\\v|\\f)`)
 
 	text = re.ReplaceAllString(text, "\n")
+
+	// text = strings.ReplaceAll(text, "\r\n", "\n")
+	// text = strings.ReplaceAll(text, "\r", "\n")
+	// text = strings.ReplaceAll(text, "\v", "\n")
+	// text = strings.ReplaceAll(text, "\f", "\n")
+
+	// text = strings.ReplaceAll(text, "\\r", "\n")
+	// text = strings.ReplaceAll(text, "\\v", "\n")
+	// text = strings.ReplaceAll(text, "\\f", "\n")
+
 	return text
 }
 
